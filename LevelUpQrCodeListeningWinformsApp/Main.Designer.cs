debbcb74d098b63e6fc4a75a99cb6a1ce5d29f55
forestb <forest.balk@gmail.com>
2018-07-04 17:16:23 -0400

    Added Winforms project

---
+++ LevelUpQrCodeListeningWinformsApp/Main.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace LevelUpQrCodeListeningWinformsApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxMain = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxMain
+            // 
+            this.richTextBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxMain.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBoxMain.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxMain.Name = "richTextBoxMain";
+            this.richTextBoxMain.ReadOnly = true;
+            this.richTextBoxMain.Size = new System.Drawing.Size(976, 680);
+            this.richTextBoxMain.TabIndex = 0;
+            this.richTextBoxMain.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 704);
+            this.Controls.Add(this.richTextBoxMain);
+            this.Name = "Main";
+            this.Text = "LevelUp QR Code Listener";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.RichTextBox richTextBoxMain;
+    }
+}
+
---
+++ LevelUpQrCodeListeningWinformsApp/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using LevelUpQrCodeListenerLibrary;
+
+namespace LevelUpQrCodeListeningWinformsApp
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            LevelUpQrCodeListener.Instance.StartListener(LevelUpPaymentTokenFound);
+        }
+
+        private void LevelUpPaymentTokenFound(string levelUpPaymentToken)
+        {
+            richTextBoxMain.AppendText($"LevelUp QR code found: {levelUpPaymentToken}{Environment.NewLine}");
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            LevelUpQrCodeListener.Instance.StopListener();
+        }
+    }
+}
